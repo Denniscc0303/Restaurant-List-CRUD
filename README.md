2f38a305b8ea8d19efd65c00ca4e02b5bea87602
Denniscc <tonynooy@gmail.com>
2021-01-22 16:51:34 +0800

    feat: project init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Restaurant List
+A simple web application for delicious restaurants
+
+## Features
+- listing movies from movie api
+- searching restaurants by title
+- watching the detail informations of restaurants
+
+### Searching
+type the keywords of the restaurants in search bar
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurant = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+  res.render('index', { restaurants: restaurant, keyword: keyword })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurants: restaurant })
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
