2ea7edc8469cafb635ac3a953d59f78e56cd48de
Denniscc <tonynooy@gmail.com>
2021-01-22 17:32:41 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose') // 載入 mongoose
+
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
---
+++ models/restaurant.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  // id: {
+  //   type: Number,
+  // },
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  name_en: String,
+  category: String,
+  image: String,
+  location: String,
+  phone: Number,
+  google_map: String,
+  rating: Number,
+  description: String
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
+
+
