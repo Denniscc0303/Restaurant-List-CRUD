7129736fd29f6e81c3cb7c7faaf2fceb9cfd9b7c
Denniscc <tonynooy@gmail.com>
2021-01-22 19:20:04 +0800

    feat: add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
-  // id: {
-  //   type: Number,
-  // },
+  id: Number,
   name: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
@@ -12,7 +10,7 @@ const restaurantSchema = new Schema({
   category: String,
   image: String,
   location: String,
-  phone: Number,
+  phone: String,
   google_map: String,
   rating: Number,
   description: String
