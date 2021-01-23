814021b68680e76cb2e6628b3f343aeb500b99ff
Denniscc <tonynooy@gmail.com>
2021-01-24 00:27:02 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+//載入所需套件及模組
 const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
-const mongoose = require('mongoose') // 載入 mongoose
-const Restaurant = require('./models/restaurant') // 載入 Todo model
-// 引用 body-parser
+const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const restaurant = require('./models/restaurant')
-
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -26,34 +26,43 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+//設定handlebars引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//使用public靜態資料
 app.use(express.static('public'))
 
+//設定根目錄路由
 app.get('/', (req, res) => {
-  Restaurant.find() // 取出 Todo model 裡的所有資料
+  Restaurant.find() // 取出 Restaurant model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
 
+//設定search路由
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurant = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
-  res.render('index', { restaurants: restaurant, keyword: keyword })
+
+  Restaurant.find() // 取出 Restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(restaurants => {
+      const restaurant = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+      res.render('index', { restaurants: restaurant, keyword: keyword })
+    }) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+  // res.render('index', { restaurants: restaurant, keyword: keyword })
 })
 
-// app.get('/restaurants/:restaurant_id', (req, res) => {
-//   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-//   res.render('show', { restaurants: restaurant })
-// })
-
+//設定new路由
 app.get('/restaurants/new', (req, res) => {
   return res.render('new')
 })
 
+//設定new post路由
 app.post('/restaurants', (req, res) => {
+  // 從 req.body 拿出表單裡的資料
   const name = req.body.name
   const name_en = req.body.name_en
   const category = req.body.category
@@ -62,7 +71,7 @@ app.post('/restaurants', (req, res) => {
   const phone = req.body.phone
   const google_map = req.body.google_map
   const rating = req.body.rating
-  const description = req.body.description       // 從 req.body 拿出表單裡的 name 資料
+  const description = req.body.description
   return Restaurant.create({
     name, name_en, category, image, location, phone, google_map, rating, description
   })     // 存入資料庫
@@ -71,6 +80,7 @@ app.post('/restaurants', (req, res) => {
 
 })
 
+//設定detail路由
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
@@ -79,6 +89,7 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//設定edit路由
 app.get('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
@@ -87,6 +98,7 @@ app.get('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//設定edit post路由
 app.post('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
   const name = req.body.name
@@ -115,6 +127,16 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//設定delete post路由
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//監聽app
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const Restaurant = require('../restaurant') // 載入 todo model
+const Restaurant = require('../restaurant')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
